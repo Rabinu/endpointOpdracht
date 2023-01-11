a99c1672f4b2437c01bc8dd60cad989aa2ee323d
Wolf <jeroenwolf86@gmail.com>
2023-01-11 15:58:00 +0100

    Test comment

--- Tests/HashTests.cs
+++ Tests/HashTests.cs
@@ -1,16 +1,18 @@
-namespace HashEndpoint.Tests;
+//   Ik ben volgens mij een beetje aan het kloten met de folder structuur en hoe de projecten in elkaar zitten dus heb deze test nog niet kunnen draaien
 
-public class HashTests
-{
-    [Fact]
-    public void TestGenerateHash()
-    {
-        string input = "test string";
-
-        var generatedHash = Hash.GenerateHash(input);
-
-        Assert.NotNull(generatedHash);
-        Assert.NotNull(generatedHash.Value);
-        Assert.Equal(64, generatedHash.Value.Length);
-    }
-}
+//  namespace HashEndpoint.Tests;
+//  
+//  public class HashTests
+//  {
+//      [Fact]
+//      public void TestGenerateHash()
+//      {
+//          string input = "test string";
+//  
+//          var generatedHash = Hash.GenerateHash(input);
+//  
+//          Assert.NotNull(generatedHash);
+//          Assert.NotNull(generatedHash.Value);
+//          Assert.Equal(64, generatedHash.Value.Length);
+//      }
+//  }
